4e72bf6132d9e2c336775a97aac061337b32a16c
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-10-16 14:00:05 -0400

    Making API request with useState, useEffect and Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <div className='App'>App</div>;
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setMonsters(response.data);
+    };
+
+    makeRequest();
+  }, []);
+
+  return (
+    <div className='App'>
+      {monsters.map((monster, idx) => (
+        <p>{monster.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default App;
