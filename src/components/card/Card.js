4db2174d6f8039150b0c484a6bc265329152ea83
Chris Davis <chris.davis5440@gmail.com>
2020-10-20 10:33:05 -0400

    Using framer-motion to animate card load

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './card.styles.css';
+import { motion } from 'framer-motion';
 
 const Card = ({ monster }) => {
   return (
-    <div className='Card'>
+    <motion.div
+      className='Card'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5 }}
+    >
       <img
         src={`http://robohash.org/${monster.id}?set=set2`}
         alt={monster.id}
       />
       <h2>{monster.name}</h2>
       <p>{monster.email}</p>
-    </div>
+    </motion.div>
   );
 };
 
